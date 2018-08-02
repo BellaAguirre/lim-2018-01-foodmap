0ef64a884b5be4fec849d5d0906f7b77e5fba5f8
BellaAguirre <yunoshe1@gmail.com>
2018-08-02 12:04:48 -0500

    mostrando restaurantes cerca

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
-var map;
-var service;
+let map;
+let service;
 var infowindow;
-
-function initMap() {
+const containerHeader = document.getElementById('title-principal');
+const containerMain = document.getElementById('container-main');
+containerHeader.style.visibility = 'hidden';
+containerMain.style.visibility ='hidden';
+window.onload = () => {
+  const container = document.getElementById('contenedor-carga');
+  setTimeout(() => {
+    container.style.visibility = 'hidden';
+    container.style.opacity = '0';
+  containerHeader.style.visibility = '';
+  containerMain.style.visibility = '';
+  }, 2000);
   
-  function localizacion (posicion) {
+}
+window.initMap = () => {
+  const localizacion = (posicion) => {
       const latitude = posicion.coords.latitude;
       const longitude = posicion.coords.longitude;
       var pyrmont = new google.maps.LatLng(latitude,longitude);
-
       map = new google.maps.Map(document.getElementById('map'), {
           center: pyrmont,
           zoom: 15
         });
-    
-      var request = {
+      var marker = new google.maps.Marker({position: {lat: latitude, lng: longitude}, map: map});
+
+      const request = {
         location: pyrmont,
         radius: '500',
         type: ['restaurant']
@@ -22,32 +34,49 @@ function initMap() {
       service = new google.maps.places.PlacesService(map);
       service.nearbySearch(request, callback);
   }
-  function error(){
-    console.log('hubo un error')
+  const error = () => {
+    console.log('hubo un error');
   }
-  navigator.geolocation.getCurrentPosition(localizacion,error);
-
+  navigator.geolocation.getCurrentPosition(localizacion, error);
 }
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      console.log(place);
-      
-      // createMarker(results[i]);
+const createMarker = (place) => {
+  const image = {
+    url: 'icono.png',
+    size: new google.maps.Size(60,60),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(15, 15)
+  };
+  // var placeLoc = place.geometry.location;
+  const marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: image,
+    title: place.name
+  });
+}
+const hola = () => {
+  alert('hola')
+}
+const viewRestaurant = (place) => {
+  console.log(place)
+  for (const key in place) {
+    if(key === 'photos') {
+      document.getElementById('place-near').innerHTML += `
+        <div class="col-4 col-xs-4 p-1">
+            <img src="${place[key][0].getUrl({'maxWidth': 200, 'maxHeight': 200})}" onclick="hola()" alt="" />            
+        </div>
+        `;
     }
   }
 }
-/* function createMarker (place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker ({
-    mapa: map,
-    posición: place.geometry.location
-  });
+const callback = (results, status) => {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      const place = results[i];
+      createMarker(place);
+      viewRestaurant(place)
+    }
+  }
+}
 
-  google.maps.event.addListener (marcador, 'click', function () {
-    infowindow.setContent (place.name);
-    infowindow.open (map, this);
-  });
-} */
